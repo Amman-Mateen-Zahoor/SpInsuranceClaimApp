9d56f370797f0adf7b197813ebf88e86b8057ec9
Amman-Mateen-Zahoor <amman.W4u@gmail.com>
2025-01-25 20:06:07 +0500

    check git working

---
+++ Src/Screens/GitTestScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GitTestScreen = () => {
+  return (
+    <View>
+      <Text>GitTestScreen</Text>
+    </View>
+  )
+}
+
+export default GitTestScreen
+
+const styles = StyleSheet.create({})
